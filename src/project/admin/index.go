4604326e1a7ad6f0b9f43f861d03dce6791a3f28
wu <william.wu@shein.com>
2018-09-28 20:12:26 +0800

    edit

--- src/project/admin/index.go
+++ src/project/admin/index.go
@@ -2,6 +2,8 @@ package admin
 
 import (
 	"github.com/astaxie/beego"
+	"project/models"
+	"fmt"
 )
 
 type IndexController struct {
@@ -9,6 +11,14 @@ type IndexController struct {
 }
 
 func (c *IndexController) Index()  {
+	sess := c.StartSession()
+	sessionValue := sess.Get("uid")
+	uid := sessionValue.(int)
+	userInfo,err := models.GetUserInfoById(uid)
+	if err!=nil{
+		fmt.Println("用户不存在")
+	}
+	c.Data["user"]=userInfo
 	//布局页面
 	c.Layout = "admin/layout.html"
 	//内容页面
--- src/project/models/use.go
+++ src/project/models/use.go
@@ -16,9 +16,21 @@ func init() {
 	orm.RegisterModel(new(User))
 }
 
+//验证用户名和密码
 func ValidateUser(name string,password string) (User,error) {
 	o := orm.NewOrm()
 	var user User
 	err := o.QueryTable("user").Filter("name",name).Filter("password",password).One(&user)
 	return user,err
 }
+//查出用户相关信息
+func GetUserInfoById(id int)(v *User,err error)  {
+	o := orm.NewOrm()
+	v = &User{Id: id}
+	if err = o.QueryTable(new(User)).Filter("id", id).RelatedSel().One(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+
